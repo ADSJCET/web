282a9f098faa1d8cca19700e2fdd2940a366cc7b
Joswin_Shogi <104709945+joswinshogi@users.noreply.github.com>
2023-10-06 15:12:34 +0530

    projects

--- src/components/image-data.ts
+++ src/components/image-data.ts
@@ -1,5 +1,7 @@
 export const images = [
-    "/adsa.png",
-    "https://d33wubrfki0l68.cloudfront.net/49de349d12db851952c5556f3c637ca772745316/cfc56/static/images/wallpapers/bridge-02@2x.png",
-    "https://d33wubrfki0l68.cloudfront.net/594de66469079c21fc54c14db0591305a1198dd6/3f4b1/static/images/wallpapers/bridge-01@2x.png"
+    "/projects/01.webp",
+    "/projects/02.webp",
+    "/projects/03.webp",
+    "/projects/04.webp",
+    "/projects/05.webp"
   ];
